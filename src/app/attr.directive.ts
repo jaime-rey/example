b64d41c38ded1b362240f310c4356e3d518ab6d0
jaime rey <jaimereycasado@gmail.com>
2023-06-07 18:12:57 +0200

    359

--- src/app/attr.directive.ts
+++ src/app/attr.directive.ts
@@ -1,16 +1,39 @@
-import { Directive, ElementRef, Input } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  SimpleChanges,
+  Output,
+  EventEmitter,
+} from '@angular/core';
+import { Product } from './product.model';
 @Directive({
   // eslint-disable-next-line @angular-eslint/directive-selector
   selector: '[pa-attr]',
 })
 export class PaAttrDirective {
-  constructor(private element: ElementRef) {}
+  constructor(private element: ElementRef) {
+    this.element.nativeElement.addEventListener('click', () => {
+      if (this.product != null) {
+        this.click.emit(this.product.category);
+      }
+    });
+  }
   @Input('pa-attr')
+  @HostBinding('class')
   bgClass: string | null = '';
-  ngOnInit() {
-    this.element.nativeElement.classList.add(
-      this.bgClass ?? 'table-success',
-      'fw-bold'
-    );
+  @Input('pa-product')
+  product: Product = new Product();
+  @Output('pa-category')
+  click = new EventEmitter<string>();
+  ngOnChanges(changes: SimpleChanges) {
+    const change = changes['bgClass'];
+    const classList = this.element.nativeElement.classList;
+    if (!change.isFirstChange() && classList.contains(change.previousValue)) {
+      classList.remove(change.previousValue);
+    }
+    if (!classList.contains(change.currentValue)) {
+      classList.add(change.currentValue);
+    }
   }
 }
