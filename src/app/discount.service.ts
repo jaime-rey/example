cdea8ab5e8beb2d821ed3ec550f4cbd384bcb5b2
jaime rey <jaimereycasado@gmail.com>
2023-06-09 02:02:23 +0200

    pg. 486

---
+++ src/app/discount.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class DiscountService {
+  private discountValue = 10;
+  public get discount(): number {
+    return this.discountValue;
+  }
+  public set discount(newValue: number) {
+    this.discountValue = newValue ?? 0;
+  }
+  public applyDiscount(price: number) {
+    return Math.max(price - this.discountValue, 5);
+  }
+}
